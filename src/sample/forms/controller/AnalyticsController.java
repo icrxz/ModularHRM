4a0dac82ffcf3b99e97753101a5693dfebe8e976
Gteixeirinha7 <gabrielteixeir137@gmail.com>
2019-11-24 12:32:15 -0300

    create analytics

--- src/sample/forms/controller/AnalyticsController.java
+++ src/sample/forms/controller/AnalyticsController.java
@@ -1,12 +1,23 @@
 package sample.forms.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
 import sample.Screens;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AnalyticsController implements Initializable {
+
+
+    @FXML
+    private javafx.scene.chart.BarChart<?, ?> BarChart;
+
+    @FXML
+    private javafx.scene.chart.AreaChart<?, ?> AreaChart;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Screens.stage.setResizable(false);
